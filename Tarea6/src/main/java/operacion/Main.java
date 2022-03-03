f88eb3c0cab8bd241daa2622a5f7aace910459ab
TanaharaXY <70725032+TanaharaXY@users.noreply.github.com>
2022-03-02 22:18:28 -0600

    Add files via upload

---
+++ Tarea6/src/main/java/operacion/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operacion;
+
+/**
+ *
+ * @author Fredo
+ */
+public class Main {
+    
+     public static void main(String[] args){
+            
+            Calif fredo = new Calif();
+            double[] calificaciones = {89,70,51,100,62};
+            fredo.setNombre("Alfredo Lara");
+            fredo.setCalificacion(calificaciones);
+            fredo.imprimeResultado();
+            
+            
+            }
+    
+}
